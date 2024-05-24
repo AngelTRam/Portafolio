7f453702f051d29ec068f721cc977f6969f734a1
Angel Tapia <angel.t.r42@gmail.com>
2024-05-24 11:32:24 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import userlogo from './usuario.png';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Nav-bar">
+          <div className='nav-left-elements'>
+            <img src={userlogo} className='User_logo'></img>
+            <h1 className='title'>Angel Tapia Ramirez</h1>
+            
+          </div>
+          <div className='nav-right-elements'>
+            <a href="#" className='Nav-link'>Home</a>
+            <a href="#" className='Nav-link'>Projects</a>
+            <a href="#" className='Nav-link'>Profile</a>
+          </div>
+        </div>
+      </header>
+      <body className='App-body'>
+        <h2 className='welcome-title'>Welcome to my site.</h2>
+        <p>Hello, my name is Angel Tapia Ramirez. I am a recently graduated
+          engineer in software development and management with a great 
+          interest in technological innovations and the software development
+           industry.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +31,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </body>
     </div>
   );
 }
