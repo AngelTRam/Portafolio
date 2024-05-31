263ddca140536ecfd284f732356582660a2b714e
Angel Tapia <angel.t.r42@gmail.com>
2024-05-31 13:40:58 -0700

    minor changes

--- src/components/social/social.js
+++ src/components/social/social.js
@@ -1,13 +1,21 @@
 import React from "react";
 import './social.css'
 //image imports
-import OcticonsMarkGitHub from './img/Octicons-mark-github.svg';
+import GitHub from './img/github.svg';
+import Gmail from './img/gmail2.svg';
+import Linkedin from './img/linkedin2.svg';
 
 const SocialIcons = () => {
     return (
         <div className="social-icons">
             <a>
-            <img className='item' src={OcticonsMarkGitHub} alt="Octicons Mark GitHub" />
+            <img className='item' src={GitHub} alt="Octicons Mark GitHub" />
+            </a>
+            <a>
+            <img className='item' src={Gmail} alt="Octicons Mark GitHub" />
+            </a>
+            <a>
+            <img className='item' src={Linkedin} alt="Octicons Mark GitHub" />
             </a>
         </div>  
     )
