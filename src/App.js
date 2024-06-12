18c0f0176235941f2fb44a473eb475876b3b6f43
Angel T <angel.t.r42@gmail.com>
2024-06-12 14:16:24 -0700

    visual update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 import './App.css';
 import Abackground from './components/animatedbackground/animatedbackground';
 import DarkMode from './components/darkmode/darkmode';
+import ProjectAccess from './components/projectsAccess/projectsAccess';
 import SocialIcons from './components/social/social';
 import userlogo from './usuario.png';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <body className='App-body'>
         <div className="Nav-bar">
           <div className='nav-left-elements'>
             <img src={userlogo} className='User_logo'></img>
             <h1 className='title'>Angel Tapia Ramirez</h1>
 
           </div>
-          <div className='nav-right-elements'>
+          <div className='nav-right-elements-desktop'>
             <a href="/" className='Nav-link'>Home</a>
             <a href="#" className='Nav-link'>Projects</a>
             <a href="#" className='Nav-link'>About me</a>
-            <DarkMode />
+          </div>
+          <div className='nav-right-elements-mobile'>
+            <a href="/" className='Nav-link'>Menu</a>
           </div>
         </div>
-      </header>
-      <body className='App-body'>
-        <div>
-          <h2 className='welcome-title'>Welcome to my site.</h2>
-          <p className='description'>Hello, my name is Angel Tapia Ramirez. I am a recently graduated
-            engineer in software development and management with a great
-            interest in technological innovations and the software development
-            industry.</p>
-          <div className='social-container'>
-            <SocialIcons />
+        <div className='body-items'>
+          <div>
+            <h2 className='welcome-title'>Welcome to my site.</h2>
+            <p className='description'>Hello, my name is Angel Tapia Ramirez. I am a recently graduated
+              engineer in software development and management with a great
+              interest in technological innovations and the software development
+              industry.</p>
+            <div className='social-container'>
+              <SocialIcons />
+            </div>
           </div>
-        </div>
-        <div>
-          AAAAAAAAAAAAAAAAAAAAA
+          <ProjectAccess />
         </div>
       </body>
     </div>
---
+++ src/components/projectsAccess/projectsAccess.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './projectAccess.css'
+
+const ProjectAccess = () => {
+    return (
+        <div className="projects">
+            <a className="project-item">Project1</a>
+            <a className="project-item">Project2</a>
+            <a className="project-item">Project3</a>
+            <a className="project-item">Project4</a>
+        </div> 
+    )
+}
+
+export default ProjectAccess;
